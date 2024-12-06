671a0d088be2157345dafe0514f98c0b4b84c324
jowi-dev <joey8williams@gmail.com>
2024-12-06 10:00:53 -0500

    flake updates

--- flake.nix
+++ flake.nix
@@ -1,32 +1,59 @@
 {
+  description = "Elixir Project for command line actions";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, flake-utils, }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let 
-      pkgs = import nixpkgs { inherit system; }; 
-      buildInputs = with pkgs; [elixir]; 
-    in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = buildInputs; 
-        };
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "banj-cli";
-          version = "0.0.0";
-          src = ./.;
-          # run tests?
-          doCheck=false;
-          inherit buildInputs;
-          buildPhase = ''
-            mix deps.get
-            mix escript.build
+
+  outputs = inputs@{ self, nixpkgs, flake-parts }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          packageName = "fnord";
+          docPath="";
+          basePackages = with pkgs; [
+            elixir 
+            erlang_27
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [inotify-tools] ;
+
+            
+          hooks = ''
+            # this allows mix to work on the local directory
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+            export PATH=$MIX_HOME/bin:$MIX_HOME/escripts:$HEX_HOME/bin:$PATH
+
+            mix local.hex --if-missing
+            export LANG=en_US.UTF-8
+            export ERL_AFLAGS="-kernel shell_history enabled"
           '';
-          installPhase = ''
-            mkdir -p $out/bin
-            mv banj $out/bin
-          '';
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = basePackages;
+            shellHook = hooks;
+          };
+
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "${packageName}";
+            version = "0.0.0";
+            src = ./.;
+            # run tests?
+            doCheck=false;
+            packages=basePackages;
+            buildPhase = ''
+              mix escript.build
+            '';
+            installPhase = ''
+              #/etc/profiles/per-user/$USER/share/doc/${packageName}/md/
+              mkdir -p $out/bin
+              mkdir -p $out/${docPath}
+              mv ${packageName} $out/bin
+            '';
+          };
         };
-      });
+    };
 }
