d175601af05682cc4e5783e9f22a817a76a2a48d
jowi-dev <joey8williams@gmail.com>
2024-12-06 09:07:18 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let 
+      pkgs = import nixpkgs { inherit system; }; 
+      buildInputs = with pkgs; [elixir] ++ additionalInputs;
+    in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = buildInputs; 
+        };
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "banj-cli";
+          version = "0.0.0";
+          src = ./.;
+          # run tests?
+          doCheck=false;
+          inherit buildInputs;
+          buildPhase = ''
+            mix deps.get
+            mix escript.build
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            mv banj $out/bin
+          '';
+        };
+      });
+}
